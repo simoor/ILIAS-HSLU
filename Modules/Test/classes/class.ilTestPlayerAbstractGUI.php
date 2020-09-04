8ab263e056495751bdc1fce987794ae7becfcf40
Stephan Winiker <stephan.winiker@hslu.ch>
2020-09-04 17:53:02 +0200

    HSLU PATCH: Temp Fix for Error in Kioskmode

--- Modules/Test/classes/class.ilTestPlayerAbstractGUI.php
+++ Modules/Test/classes/class.ilTestPlayerAbstractGUI.php
@@ -1961,12 +1961,7 @@ abstract class ilTestPlayerAbstractGUI extends ilTestServiceGUI
      */
     protected function populateHelperGuiContent($helperGui)
     {
-        if ($this->object->getKioskMode()) {
-            $this->tpl->addBlockfile($this->getContentBlockName(), 'content', "tpl.il_as_tst_kiosk_mode_content.html", "Modules/Test");
-            $this->tpl->setContent($this->ctrl->getHTML($helperGui));
-        } else {
             $this->tpl->setVariable($this->getContentBlockName(), $this->ctrl->getHTML($helperGui));
-        }
     }
     
     /**
