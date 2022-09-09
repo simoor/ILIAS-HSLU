bc32dfb8dabb5038e4587fb4e6d46a6c26333c83
Stephan Kergomard <webmaster@kergomard.ch>
2022-09-09 11:44:37 +0300

    PATCH HSLU Make Interactive Video Work with WAC
    See README.md of Interactive Video

--- Services/MediaObjects/classes/class.ilObjMediaObject.php
+++ Services/MediaObjects/classes/class.ilObjMediaObject.php
@@ -1219,6 +1219,9 @@ class ilObjMediaObject extends ilObject
 
             // Media Pool
             case "mep":
+// BEGIN PATCH HSLU Make Interactive Video work with Access Checker
+            case "xvid":
+// END PATCH HSLU Make Interactive Video work with Access Checker
                 $obj_id = $id;
                 break;
 
