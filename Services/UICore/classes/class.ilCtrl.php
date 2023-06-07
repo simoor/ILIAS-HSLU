c025c83aaa4bf7b31b6539ee36bbc467883d402e
msalterhslu <mark.salter@hslu.ch>
2023-06-07 11:23:17 +0200

    PATCH HSLU if setTargetScript is public then a public getTargetScript should also be available (was available pre-8.0)

--- Services/UICore/classes/class.ilCtrl.php
+++ Services/UICore/classes/class.ilCtrl.php
@@ -705,6 +705,14 @@ class ilCtrl implements ilCtrlInterface
     /**
      * @inheritDoc
      */
+    public function getTargetScript(): string
+    {
+        return $this->context->getTargetScript();
+    }
+
+    /**
+     * @inheritDoc
+     */
     public function isAsynch(): bool
     {
         return $this->context->isAsync();
