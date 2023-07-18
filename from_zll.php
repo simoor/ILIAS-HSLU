078629100d8c17321226f66351e88ac8a80045f8
Raphael heer <raphael.heer@hslu.ch>
2023-07-18 23:27:36 +0000

    TEMP PATCH HSLU: Temporary fix from_zll cross-login

--- from_zll.php
+++ from_zll.php
@@ -9,14 +9,16 @@
 *
 * This script allows login posts from other pages
 */
-
 require_once("Services/Init/classes/class.ilInitialisation.php");
 
-$_REQUEST['baseClass'] = 'ilStartUpGUI';
-$_GET['client_id'] = 'hslu';
-$_REQUEST['cmd']['doStandardAuthentication'] = 'Anmelden';
+ilInitialisation::initILIAS();
+/** @var $session ilAuthSession */
+if(isset($DIC['ilAuthSession'])) {
+    $session = $DIC['ilAuthSession'];
+    $session->logout();
+}
 
-ilInitialisation::initIlias();
+ilInitialisation::reinitILIAS();
 
 $ilCtrl->initBaseClass("ilStartUpGUI");
 $ilCtrl->setCmd('doStandardAuthentication');
