66d4fc54a6aa2feeee8d9a559dfff2c81104ccf9
msalterhslu <mark.salter@hslu.ch>
2023-10-09 16:02:09 +0200

    TEMP PATCH HSLU: Test points calculated as floats not integers

--- Modules/Test/classes/class.ilObjTest.php
+++ Modules/Test/classes/class.ilObjTest.php
@@ -4683,7 +4683,6 @@ class ilObjTest extends ilObject implements ilMarkSchemaAware, ilEctsGradesEnabl
 
         if ($question_id > 0) {
             $question->object->loadFromDb($question_id);
-
             global $DIC;
             $ilCtrl = $DIC['ilCtrl'];
             $ilDB = $DIC['ilDB'];
