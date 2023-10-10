cdb8c1b38f1cce883924ecfd83aae4b85416ae97
msalterhslu <mark.salter@hslu.ch>
2023-10-10 10:52:25 +0200

    TEMP PATCH HSLU: prevent $ not defined error

--- Services/COPage/classes/class.ilPCMediaObject.php
+++ Services/COPage/classes/class.ilPCMediaObject.php
@@ -25,6 +25,7 @@ use ILIAS\MediaObjects\Usage\UsageDBRepository;
  */
 class ilPCMediaObject extends ilPageContent
 {
+    protected ilCtrlInterface $ctrl;
     protected UsageDBRepository $mob_usage_repo;
     protected php4DOMElement $mal_node;
     protected ilObjUser $user;
@@ -46,9 +47,10 @@ class ilPCMediaObject extends ilPageContent
         $this->lng = $DIC->language();
         $this->global_tpl = $DIC['tpl'];
         $this->mob_usage_repo = $DIC->mediaObjects()
-            ->internal()
-            ->repo()
-            ->usage();
+                                    ->internal()
+                                    ->repo()
+                                    ->usage();
+        $this->ctrl = $DIC->ctrl();
     }
 
     public function readMediaObject(int $a_mob_id = 0): void
@@ -476,7 +478,13 @@ class ilPCMediaObject extends ilPageContent
 
         // async ensures to have onloadcode of modal in output
         // if other main tpl is used, see #32198
-        return $a_output . "<div class='il-copg-mob-fullscreen-modal'>" . $this->ui->renderer()->renderAsync($modal) . "</div>";
+        // note: if always rendered async, $ not defined errors will be thrown in non-async cases
+        if ($this->ctrl->isAsynch()) {
+            $html = $this->ui->renderer()->renderAsync($modal);
+        } else {
+            $html = $this->ui->renderer()->render($modal);
+        }
+        return $a_output . "<div class='il-copg-mob-fullscreen-modal'>" . $html . "</div>";
     }
 
     public function getOnloadCode(string $a_mode): array
@@ -570,9 +578,9 @@ class ilPCMediaObject extends ilPageContent
         global $DIC;
 
         $mob_usage_repo = $DIC->mediaObjects()
-            ->internal()
-            ->repo()
-            ->usage();
+                              ->internal()
+                              ->repo()
+                              ->usage();
 
         $log = ilLoggerFactory::getLogger("copg");
 
