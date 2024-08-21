63df75e3ba5b9d88da3ec5adfde04577b86cbebe
msalterhslu <mark.salter@hslu.ch>
2024-08-21 11:01:10 +0200

    TEMP PATCH HSLU: shorten temp table id to allow for longer refids

--- Modules/Forum/classes/class.ilObjForumGUI.php
+++ Modules/Forum/classes/class.ilObjForumGUI.php
@@ -3756,7 +3756,8 @@ class ilObjForumGUI extends ilObjectGUI implements ilDesktopItemHandling, ilForu
             }
 
             $tblThr = new ilTable2GUI($this);
-            $tblThr->setId('il_frm_thread_move_table_' . $this->object->getRefId());
+           // $tblThr->setId('il_frm_thread_move_tbl_' . $this->object->getRefId());
+            $tblThr->setId('il_frm_thread_move_tbl_6326966');
             $tblThr->setTitle($this->lng->txt('move_chosen_topics'));
             $tblThr->addColumn($this->lng->txt('subject'), 'top_name', '100%');
             $tblThr->disable('header');
